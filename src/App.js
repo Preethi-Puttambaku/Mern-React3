19b0a2dff067c9d7797880f9877377af9aa86c3b
Preethi- Puttambaku <preethiputtambaku21@gmail.com>
2023-10-14 23:01:07 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const Colors = ({ onColorClick }) => {
+  const colorClickHandler = (color) => {
+    onColorClick(color); // Call the parent component's handler function
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>
+      <li className="colors" id="color1" onClick={() => colorClickHandler('red')}></li>
+      <li className="colors" id="color2" onClick={() => colorClickHandler('lawngreen')}></li>
+      <li className="colors" id="color3" onClick={() => colorClickHandler('blue')}></li>
+      <li className="colors" id="color4" onClick={() => colorClickHandler('yellow')}></li>
+      <li className="colors" id="color5" onClick={() => colorClickHandler('violet')}></li>
+      <li className="colors" id="color6" onClick={() => colorClickHandler('skyblue')}></li>
+      <li className="colors" id="color7" onClick={() => colorClickHandler('orange')}></li>
+      <li className="colors" id="color8" onClick={() => colorClickHandler('darkviolet')}></li>
+      <li className="colors" id="color9" onClick={() => colorClickHandler('palevioletred')}></li>
+      <li className="colors" id="color10" onClick={() => colorClickHandler('green')}></li>
+      <li className="colors" id="color11" onClick={() => colorClickHandler('brown')}></li>
+      <li className="colors" id="color12" onClick={() => colorClickHandler('black')}></li>
+    </ul>
   );
 }
 
-export default App;
+export default Colors;
---
+++ src/colour.js
@@ -0,0 +1,76 @@
+.container {
+    width: 800px;
+    height: 500px;
+    background-color: rgb(175, 161, 161);
+    margin: 100px auto; 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .color {
+    width: 600px;
+    height: 400px;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .pick {
+    background-color: rgb(59, 160, 59);
+    font-weight: bold;
+    border-width: 3px;
+    width: 150px;
+    height: 40px;
+    border-radius: 4px;
+  }
+  
+  .pick:hover {
+    background-color: rgb(102, 213, 102);
+    cursor: pointer;
+  }
+  
+  .colors {
+    width: 30px;
+    height: 30px;
+    margin: 5px;
+    display: inline-block;
+  }
+  
+  #color1 {
+    background-color: red;
+  }
+  #color2 {
+    background-color: lawngreen;
+  }
+  #color3 {
+    background-color: blue;
+  }
+  #color4 {
+    background-color: yellow;
+  }
+  #color5 {
+    background-color: violet;
+  }
+  #color6 {
+    background-color: skyblue;
+  }
+  #color7 {
+    background-color: orange;
+  }
+  #color8 {
+    background-color: darkviolet;
+  }
+  #color9 {
+    background-color: palevioletred;
+  }
+  #color10 {
+    background-color: green;
+  }
+  #color11 {
+    background-color: brown;
+  }
+  #color12 {
+    background-color: black;
+  }
